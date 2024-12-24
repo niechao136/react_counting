574e0986d958522428ddf0feffa2667f084cb039
nie.chao <nie.chao@advantech.com.cn>
2024-12-24 13:07:14 +0800

    config alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ios.js', '.android.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/test.ts
@@ -0,0 +1,2 @@
+
+export const str = 'alias test';
